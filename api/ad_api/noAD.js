b5399611a6df6199e2f36bf2f565c79ecf3e55c6
wusiqing <siqingiii@163.com>
2019-10-17 11:58:02 +0800

    更新

--- api/ad_api/noAD.js
+++ api/ad_api/noAD.js
@@ -2,11 +2,18 @@
     // adf+数字
     var adNode = document.getElementsByClassName('afd-ad')
     var adBottomNode = document.getElementsByClassName('banner')
-
+    var bottomOperateTop = document.getElementsByClassName('bottomOperateTop')
     for(var i = 0; i < adNode.length; i++){
         adNode[i].style.display = 'none';
     }
     for(var j = 0; j < adBottomNode.length; j++){
         adBottomNode[j].style.display = 'none'
     }
+    for(var k = 0; k < bottomOperateTop.length; k++){
+        bottomOperateTop[k].style.display = 'none'
+    }
 })()
+
+// 使用
+// https://heycqing.github.io/blog/api/ad_api/noAD.js
+// javascript:(function(){var s = document.createElement('script'); s.type = 'text/javascript'; s.src = 'https://heycqing.github.io/blog/api/ad_api/noAD.js'; document.body.insertBefore(s, document.body.firstChild); alert('点击除去😡广告')})()
