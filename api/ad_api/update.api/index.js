b23d6e0bffd1628691ff8c2ab0fa07f785e98851
wusiqing <siqingiii@163.com>
2019-12-29 14:27:51 +0800

    fix : bug

--- api/ad_api/update.api/index.js
+++ api/ad_api/update.api/index.js
@@ -46,18 +46,23 @@
     removeADRoot.prototype.stopAD = function(key) {
         // 针对 手机百度小说的广告
         let maxLength = this.maxLength()
+        console.log('maxLength:', maxLength)
         for(var i = 0; i < maxLength; i++){
             if(this.adNode[i]){
+                console.log('执行')
                 this.adNode[i].style.display = 'none'
             }
             if(this.adBottomNode[i]){
                 // if(this.adBottomNode[i].style.display !== 'none'){
                 //     return true
                 // }else{
+                    console.log('执行')
                     this.adBottomNode[i].style.display = 'none'
                 // }
             }
             if(this.bottomOperateTop[i]){
+                console.log('执行')
+                
                 // if(this.bottomOperateTop[i].style.display !== 'none'){
                 //     return true
                 // }else{
@@ -69,6 +74,7 @@
 
     removeADRoot.prototype.init = function() {
         var self = this
+        console.log('pppp')
         this.setvalue()
         this.stopAD()
         var originHeight =  document.body.clientHeight ||document.body.scrollHeight;
